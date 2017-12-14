28294f47e3f861e534d9d39ce4e84fa57d02eb14
andresCedeno26 <33564537+andresCedeno26@users.noreply.github.com>
2017-12-13 23:13:44 -0500

    1

--- Comisariato/Comisariato/Formularios/Informes/FrmKardex.cs
+++ Comisariato/Comisariato/Formularios/Informes/FrmKardex.cs
@@ -47,11 +47,7 @@ namespace Comisariato.Formularios.Informes
             DataTable datosVenta = objConsulta.BoolDataTable("select e.SUCURSAL, e.CAJA, e.NFACTURA, d.PRECIO, d.CANTIDAD, e.FECHA" +
             " from TbEncabezadoFactura e, TbDetalleFactura d, TbProducto p where e.IDFACTURA = d.NFACTURA and" +
             " d.CODIGOBARRAPRODUCTO = p.CODIGOBARRA and p.CODIGOBARRA = 123456789 and e.FECHA between  '" + Convert.ToString(dtpDesde.Value) + "' and '" + Convert.ToString(dtpHasta.Value) + "' order by e.FECHA");
-
-            //int cantidadCompras = datosCompra.Rows.Count, cantidadVentas = datosVenta.Rows.Count, contadorCompra = 0, contadorventa = 0;
-            //bool quienMayor = false;
-            //int filaDGV = 0;
-            //int limiteGeneral = cantidadCompras + cantidadVentas;
+            
             for (int i = 0; i < datosCompra.Rows.Count; i++)
             {
                 DataRow dtCompra = datosCompra.Rows[i];
@@ -64,20 +60,60 @@ namespace Comisariato.Formularios.Informes
                 {
                     dgvKardex.Rows.Add();
                 }
-                dgvKardex.Rows.Insert(1,1);
+                //dgvKardex.Rows.Insert(1,1);
             }
             
             for (int i = 0; i <datosVenta.Rows.Count; i++)
             {
+                int j = 0;
+                bool banderaIngreso = false;
                 DataRow dtVenta = datosVenta.Rows[i];
-                for (int j = 0; j < dgvKardex.RowCount -1; j++)
+                for (j = 0; j < dgvKardex.RowCount - 1; j++)
                 {
                     if (Convert.ToDateTime(dtVenta["FECHA"]) < Convert.ToDateTime(dgvKardex.Rows[j].Cells[0].Value))
                     {
-                        
+                        int numeroFactura = Convert.ToInt32(dtVenta["NFactura"]), serie1 = Convert.ToInt32(dtVenta["SUCURSAL"]), serie2 = Convert.ToInt32(dtVenta["CAJA"]);
+                        dgvKardex.Rows.Insert(j, 1);
+                        dgvKardex.Rows[j].Cells[0].Value = dtVenta["FECHA"];
+                        dgvKardex.Rows[j].Cells[1].Value = "Venta S/F " + serie1.ToString("D3") + serie2.ToString("D3") + numeroFactura.ToString("D9");
+                        dgvKardex.Rows[j].Cells[5].Value = dtVenta["CANTIDAD"];
+                        dgvKardex.Rows[j].Cells[6].Value = dtVenta["PRECIO"];
+                        dgvKardex.Rows[j].Cells[7].Value = Convert.ToSingle(dgvKardex.Rows[j].Cells[3].Value) * Convert.ToInt32(dgvKardex.Rows[j].Cells[2].Value);
+                        banderaIngreso = true;
+                        break;
                     }
-                }                
-            }           
+                }
+                if (!banderaIngreso)
+                {
+                    if (j == dgvKardex.RowCount - 1)
+                    {
+                        dgvKardex.Rows.Add();
+                    }
+                    int numeroFactura = Convert.ToInt32(dtVenta["NFactura"]), serie1 = Convert.ToInt32(dtVenta["SUCURSAL"]), serie2 = Convert.ToInt32(dtVenta["CAJA"]);
+                    //dgvKardex.Rows.Insert(, 1);
+                    dgvKardex.Rows[j].Cells[0].Value = dtVenta["FECHA"];
+                    dgvKardex.Rows[j].Cells[1].Value = "Venta S/F " + serie1.ToString("D3") + serie2.ToString("D3") + numeroFactura.ToString("D9");
+                    dgvKardex.Rows[j].Cells[5].Value = dtVenta["CANTIDAD"];
+                    dgvKardex.Rows[j].Cells[6].Value = dtVenta["PRECIO"];
+                    dgvKardex.Rows[j].Cells[7].Value = Convert.ToSingle(dgvKardex.Rows[i].Cells[3].Value) * Convert.ToInt32(dgvKardex.Rows[i].Cells[2].Value);
+                }
+            }
+        }
+        //calcular la existencia
+        public void calculoExistecia()
+        {
+            for (int i = 0; i < dgvKardex.RowCount -1; i++)
+            {
+                string[] tipo = Convert.ToString(dgvKardex.Rows[i].Cells[1].Value).Split(' ');
+                if (i == 0)
+                {
+                    if (tipo[0] == "Compra")
+                    {
+
+                    }
+                    //dgvKardex.Rows[8].Cells[0].Value = 
+                }
+            }
         }
     }
 }
